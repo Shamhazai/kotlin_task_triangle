3cdc2d71d598878cb92b533f615c784bb2c59586
Maria Pshenichinkova <annaka4356@gmail.com>
2023-03-22 01:09:12 +0300

    Add task

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,41 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.math.abs
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    // Ввод координат треугольника
+    println("Введите координаты вершин треугольника, каждую чиселку с новой строчки (x1 y1 x2 y2 x3 y3):")
+    try {
+        val x1 = readln().toDouble()
+        val y1 = readln().toDouble()
+        val x2 = readln().toDouble()
+        val y2 = readln().toDouble()
+        val x3 = readln().toDouble()
+        val y3 = readln().toDouble()
+
+        // Ввод координат точки
+        println("Введите координаты точки (x y):")
+        val x = readln().toDouble()
+        val y = readln().toDouble()
+//        ?: run {
+//            println("Ошибка: неверный формат ввода")
+//            return
+//        }
+
+        // Вычисление площади треугольника
+        val area = 0.5 * Math.abs((x2 - x1) * (y3 - y1) - (x3 - x1) * (y2 - y1))
+
+        // Вычисление площадей треугольников, образованных точкой
+        val area1 = 0.5 * abs((x1 - x) * (y2 - y) - (x2 - x) * (y1 - y))
+        val area2 = 0.5 * abs((x2 - x) * (y3 - y) - (x3 - x) * (y2 - y))
+        val area3 = 0.5 * abs((x3 - x) * (y1 - y) - (x1 - x) * (y3 - y))
+
+        // Проверка, находится ли точка внутри треугольника
+        if (area == area1 + area2 + area3) {
+            println("Точка находится внутри треугольника")
+        } else {
+            println("Точка находится вне треугольника")
+        }
+    }
+    catch (e:NumberFormatException) {
+        println("Ойй, неправильный формат..")
+    }
 }
